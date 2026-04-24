af360f3a5f47cff7b2badacb940506f465a2f414
Habeeb Kazeem <hkazeem@terpmail.umd.edu>
2026-04-23 23:01:28 -0400

    Fix merge conflict in rounds.py

--- rounds.py
+++ rounds.py
@@ -1,3 +1,12 @@
+
+class Round:
+    def __init__(self, round_number):
+        self.round_number = round_number
+        
+    def generate_round(self):
+        pass
+
+
 class RoundManager:
     """Class to manage rounds and enemy waves in the game"""
 
