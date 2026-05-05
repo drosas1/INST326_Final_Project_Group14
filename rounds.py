2a48222fceb157eea57fbb0f71495a0f49749f66
habeeb.kazeem <hkazeem@terpmail.umd.edu>
2026-05-05 10:40:54 -0400

    Update rounds.py

--- rounds.py
+++ rounds.py
@@ -6,7 +6,25 @@ class Round:
         
     def generate_round(self):
         pass
+    def __init__(self, round_number):
+        """Initialize a Round with a given round number.
 
+        Args:
+            round_number (int): The round number to initialize.
+        """
+        self.round_number = round_number
+        self.enemies = []
+    def generate_enemies(self):
+        """ Generates a list of enemies thats scaled to the round number.
+
+        Uses a list comprehension to create Enemy objects. Each round increases
+        the number of enemies and scales their health and attack power based on 
+        the round number.
+
+        Returns:
+            list: A list of Enemy objects for this round.
+
+        """
 
 class RoundManager:
     """Class to manage rounds and enemy waves in the game"""
